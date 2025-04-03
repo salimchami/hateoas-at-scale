0da125ab1cb93d3b5a3974031a23257b0003f353
sch <it@salimchami.net>
2025-04-03 17:07:56 +0200

    refactor cart service and enable modularized architecture

--- cart/src/main/kotlin/com/hateoasatscale/cart/infrastructure/driven/adapters/providers/users/ProviderUserDto.kt
+++ cart/src/main/kotlin/com/hateoasatscale/cart/infrastructure/driven/adapters/providers/users/ProviderUserDto.kt
@@ -1,7 +1,7 @@
 package com.hateoasatscale.cart.infrastructure.driven.adapters.providers.users
 
 class ProviderUserDto(
-    val username: String,
+    private val username: String,
     val firstname: String,
     val lastname: String
 )
