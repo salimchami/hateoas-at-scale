93da09a290a2b8bcc3a80a0cd1af1b1ab546cb12
sch <it@salimchami.net>
2025-04-16 20:03:02 +0200

    Update client theme and layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,33 +1,17 @@
-import {Component, inject} from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {Component} from '@angular/core';
+import {LayoutComponent} from './layout/layout.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule,
-    MatSidenavModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatInputModule,],
+  imports: [
+    LayoutComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   title = 'client';
-  private _formBuilder = inject(FormBuilder);
-
-  options = this._formBuilder.group({
-    bottom: 0,
-    fixed: true,
-    top: 0,
-  });
 }
