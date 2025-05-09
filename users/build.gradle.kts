d0e5be1b2f88530ff93cdc841ec394f642c41f8e
sch <it@salimchami.net>
2025-05-09 12:27:08 +0200

    add oauth2 resource server security configuration for cart products and users services

--- users/build.gradle.kts
+++ users/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.springframework.security:spring-security-test")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
